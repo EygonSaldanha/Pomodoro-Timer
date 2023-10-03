03bf51ce6e292c850bb130f50afc2f6e8ecafd02
eygon.saldanha <eygon.saldanha@dataprev.gov.br>
2023-10-03 01:34:07 -0300

    DefaultLayout home pages routes Header

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,14 @@
-import {} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
+import { Home } from './pages/home'
 export function Router() {
-    return ()
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
 }
